c186f22ccf964b4174915a40d80e8f387d8d5771
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-09-21 12:57:56 +0500

    facebook test

--- Evolve/src/main/java/com/evolvear/evolve/Tester.java
+++ Evolve/src/main/java/com/evolvear/evolve/Tester.java
@@ -1,13 +1,31 @@
 package com.evolvear.evolve;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class Tester {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
 
     public static void ShowToast(Context context, String msg)
     {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onActivityResult(int requestCode, int responseCode, Intent intent){
+//        if (requestCode == RC_SIGN_IN) {
+//            if (responseCode != ((Activity)ctx).RESULT_OK) {
+//                setSignInClicked(false);
+//            }
+//            setIntentInProgress(false);
+//            reconnect();
+//        }
     }
 
 }
--- app/src/main/java/com/evolvear/evolvelibrary/MainActivity.java
+++ app/src/main/java/com/evolvear/evolvelibrary/MainActivity.java
@@ -5,10 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.evolvear.evolve.Facebook;
 import com.evolvear.evolve.Tester;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity  implements Facebook.FacebookResponseListener, View.OnClickListener {
+
+    private Facebook facebook;
+    private Button fbLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +25,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Tester.ShowToast(this,"Wow We did it");
+
+        initView();
+        facebook = new Facebook(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_fb:
+                facebook.login();
+                break;
+//            case R.id.linkedInLoginButton:
+//                try {
+//                    linkedIn.login();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//                break;
+//            case R.id.googleLoginButton:
+//                google.login();
+//                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+//        if (requestCode == SocialLoginConstant.GOOGLE_REQUEST_CODE) {
+//            google.activityResult(requestCode, resultCode, data);
+//        } else {
+            facebook.activityResult(requestCode, resultCode, data);
+//        }
+    }
+
+    @Override
+    public void onFacebookResponseListener(JSONObject response, boolean error) {
+        Log.d("Response", String.valueOf(response));
+    }
+
+    private void initView() {
+        fbLoginButton = findViewById(R.id.btn_fb);
+        fbLoginButton.setOnClickListener(this);
     }
 }
